db198d43df2752c169f0ccac24c77e807a31e5ba
JojoBaPb <sipo.jmudenda@gmail.com>
2023-04-12 16:38:27 -0700

    9fix

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -6,16 +6,16 @@ class Student:
     """Represents a student."""
 
     def __init__(self, first_name, last_name, age):
-    """Init a new Student.
+        """Init a new Student.
 
-    Args:
-    first_name (str): first name of the student.
-    last_name (str):  last name of the student.
-    age (int): age of the student.
-    """
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
+        Args:
+            first_name (str): first name of the student.
+            last_name (str):  last name of the student.
+            age (int): age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self):
         """Gets a dictionary of the Student."""
