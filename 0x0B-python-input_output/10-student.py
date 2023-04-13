ec09d7f2263554f143f69493071db3648300c83c
JojoBaPb <sipo.jmudenda@gmail.com>
2023-04-13 09:50:07 -0700

    10fix

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -19,7 +19,7 @@ class Student:
 
     def to_json(self, attrs=None):
         """Get a dictionary representation of the Student.
-        
+
         If attrs is a list of strings, represents only attris
         included in list.
 
