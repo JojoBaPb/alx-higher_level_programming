6e293b3df05278f48f68bbaa46bfe91236faf674
JojoBaPb <sipo.jmudenda@gmail.com>
2023-04-13 17:47:37 -0700

    101

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,7 +5,7 @@ contains MyList class
 
 
 class MyList(list):
-    """enables printing for the built in list class."""
+    """Enables printing for the built in list class."""
 
     def print_sorted(self):
         """Prints a list in ascending order."""
