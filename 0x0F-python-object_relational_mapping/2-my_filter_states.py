6bea2e4a50ec77ec472e65aaa1bfc680639b0cdc
JojoBaPb <sipo.jmudenda@gmail.com>
2023-06-24 01:38:30 +0200

    2-3fix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -3,16 +3,16 @@
 Displays all values in the states table of the database hbtn_0e_0_usa
 where name matches that supplied by argument.
 Usage 4 args: ./2-my_filter_states.py <mysql username> \
-        <mysql password> \
-        <database name> \
-        <state name searched>
+                                       <mysql password> \
+                                       <database name> \
+                                       <state name searched>
 """
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], port=3306, host="localhost",
-            passwd=sys.argv[2], db=sys.argv[3])
+                         passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
     id ASC".format(sys.argv[4]))
