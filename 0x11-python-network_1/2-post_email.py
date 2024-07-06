04e70141bed55e27d76584eba86377e33027bab8
JojoBaPb <sipo.jmudenda@gmail.com>
2024-07-06 02:44:14 +0200

    readme+mandatory+optional

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""A script that: takes in a URL
+- sends a POST request to passed URL
+- takes email as param
+- displays body of response
+"""
+import sys
+import urllib.parse
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
